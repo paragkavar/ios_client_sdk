22388e064adb210327caf180ad75d5f7ffcd8c53
Michael Berkovich <michael@geni.com>
2012-01-10 14:40:24 -0800

    Initial Commit

---
+++ OAuthX/OAuthX.h
@@ -0,0 +1,13 @@
+//
+//  OAuthX.h
+//  OAuthX
+//
+//  Created by Michael Berkovich on 1/10/12.
+//  Copyright (c) 2012 Geni.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OAuthX : NSObject
+
+@end
---
+++ OAuthX/OAuthX.m
@@ -0,0 +1,23 @@
+//
+//  OAuthX.m
+//  OAuthX
+//
+//  Created by Michael Berkovich on 1/10/12.
+//  Copyright (c) 2012 Geni.com. All rights reserved.
+//
+
+#import "OAuthX.h"
+
+@implementation OAuthX
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
